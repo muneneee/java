55b2331346ab35b8c9dc8a28ba506b573f4e5e0b
Kevin Munene <lukekevin39@gmail.com>
2020-02-16 17:17:47 +0300

    add changes to html

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,79 @@
-var form = document.getElementById("form-id");
-
-document.getElementById("your-id").addEventListener("click",function (){
-  form.submit();
-});
-const maleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-const femaleNames =["Akosua", "Adwoa", "Abenaa", "Yaa", "Afua", "Ama"];
-
-function giveAkanName (){
-  var year =parseint(document.getElementById("year").value);
-  var month =parseint(document.getElementById("month").value);
-  var century =parseint(document.getElementById("century").value);
-  var date =parseint(document.getElementById("date").value);
-  var gender =(document.getElementById("Gender").text);
+<script type="text/javascript">
+function validation(){
+var date = document.getElementById('date').value;
+var month = document.getElementById('month').value;
+var year = document.getElementById('year').value;
+if (date>'31'|| date<'1')
+{
+  alert('inavlid');
+  return false;
 }
-var day = ((("century" / 4 - 2 * "century" - 1) + ((5 * "year" / 4) + ((26 * ("month" + 1) / 10)) + "day")% 7;{
-  return day;
+else {
+    return true
 }
+if (month>'12'|| month<'1')
+{
+  alert('inavlid');
+  return false;
+}
+else {
+    return true
+    if (day ==='0' && Gender === 'Male')
+    {
+      alert('your akan name is Kwasi');
+      return true;
+    }
+    else if (day ==='0' && Gender === 'Female') {
+      alert('your akan name is Akasua');
+      return true;
+    }
+    if (day ==='1' && Gender === 'Male')
+    {
+      alert('your akan name is Kwadwo');
+      return true;
+    }
+    else if (day ==='1' && Gender === 'Female') {
+      alert('your akan name is Adwaa');
+      return true;
+    if (day ==='2' && Gender === 'Male')
+    {
+      alert('your akan name is Kwabena');
+      return true;
+    }
+    else if (day ==='2' && Gender === 'Female') {
+      alert('your akan name is Abena');
+      return true;
+    if (day ==='3' && Gender === 'Male')
+    {
+      alert('your akan name is Kwaku');
+      return true;
+    }
+    else if (day ==='3' && Gender === 'Female') {
+      alert('your akan name is Akua');
+      return true;
+    if (day ==='4' && Gender === 'Male')
+    {
+      alert('your akan name is Yaw');
+      return true;
+    }
+    else if (day ==='4' && Gender === 'Female') {
+      alert('your akan name is Yaa');
+      return true;
+    if (day ==='5' && Gender === 'Male')
+    {
+      alert('your akan name is Kofi');
+      return true;
+    }
+    else if (day ==='5' && Gender === 'Female') {
+      alert('your akan name is Afua');
+      return true;
+    if (day ==='6' && Gender === 'Male')
+    {
+      alert('your akan name is Kwame');
+      return true;
+    }
+    else if (day ==='6' && Gender === 'Female') {
+      alert('your akan name is Ama');
+      return true;
+    }
+    </script>
