ae1c4fc16f2ce3896c85a2361af8f4bc608cb029
Kevin Munene <lukekevin39@gmail.com>
2020-02-16 15:01:26 +0300

    add if statements

--- js/scripts.js
+++ js/scripts.js
@@ -5,4 +5,12 @@ document.getElementById("your-id").addEventListener("click",function (){
 });
 const maleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 const femaleNames =["Akosua", "Adwoa", "Abenaa", "Yaa", "Afua", "Ama"];
-const daysOfTheWeek =[]
+
+function giveAkanName (){
+  var year =parseint(document.getElementById("year").value);
+  var month =parseint(document.getElementById("month").value);
+  var century =parseint(document.getElementById("century").value);
+  var date =parseint(document.getElementById("date").value);
+  var gender =(document.getElementById("Gender").text);
+}
+var day = ((("century" / 4 - 2 * "century" - 1) + ((5 * "year" / 4) + ((26 * ("month" + 1) / 10)) + "day")% 7;
